2973d6dc9800d9e7c1716d0e9f776d5280d25c4d
carbon-hvze <6391474+carbon-hvze@users.noreply.github.com>
2023-04-13 13:08:33 +0300

    comment desktop app init

--- src/zd/web_ui.clj
+++ src/zd/web_ui.clj
@@ -21,20 +21,18 @@
           (.launch (into-array String [])))}
   'ok)
 
-(def app (proxy [Application] []
-           (start [app]
-             (Platform/runLater (fn [] #_(start-app app {:uri "http://localhost:4444"}))))))
-
-(comment
-  (.stop app)
-
-  (Platform/exit)
-
-  (Application/launch (class app) (into-array String [])))
-
-
-
 (defmethod zen/stop 'zd/web-ui
   [ztx config {a :app :as state}]
   (println :stopping-web-ui)
   #_(.stop a (into-array String [])))
+
+(comment
+  (.stop app)
+
+  (def app (proxy [Application] []
+             (start [app]
+               (Platform/runLater (fn [] #_(start-app app {:uri "http://localhost:4444"}))))))
+
+  (Platform/exit)
+
+  (Application/launch (class app) (into-array String [])))
