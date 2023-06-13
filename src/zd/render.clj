9a5d9dde45730c6d38638ed38a24d5ab292fcbb6
carbon-hvze <6391474+carbon-hvze@users.noreply.github.com>
2023-06-13 19:28:49 +0300

    remove width

--- src/zd/render.clj
+++ src/zd/render.clj
@@ -87,7 +87,7 @@
            (methods/renderkey ztx ctx err-block)))))
 
 (defn render-blocks [ztx ctx {m :zd/meta subs :zd/subdocs :as doc} & [render-subdoc?]]
-  [:div {:class (c [:w "60rem"])}
+  [:div #_{:class (c [:w "60rem"])}
    ;; TODO render errors in doc view
    (when-let [errs (seq (:errors m))]
      (methods/renderkey ztx ctx {:data errs :ann {} :key :zd/errors}))
